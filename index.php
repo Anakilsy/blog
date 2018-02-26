2d8a61ee6ae8f1f674a8f0da5c68df64e6c73dec
anakilsy <anita1997angel@gmail.com>
2018-02-26 11:58:52 +0100

    parte

--- index.php
+++ index.php
@@ -8,13 +8,24 @@
 
 <body>
 <div class="cabecera">
+    <div class="contenidocabecera">
+        
+    </div>
     
 </div>
-<div class="cuerpo">
+<div class="fondo">
+    <div class="contenido">
+        
+    </div>
     
 </div>
 
-
+<div class="pie" >
+    <div class="contenidopie">
+        
+    </div>
+    
+</div>
 </body>
 </html>
 
