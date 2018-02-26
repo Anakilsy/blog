56a5ee5fdfb5e8c59d0f375d49c2ce8e8c764d93
anakilsy <anita1997angel@gmail.com>
2018-02-26 14:06:42 +0100

    cabecera y casi pie

--- index.php
+++ index.php
@@ -9,7 +9,8 @@
 <body>
 <div class="cabecera">
     <div class="contenidocabecera">
-        
+        <a href="http://www.ajeclm.com/" target="_blank" class="logo"></a>
+        <a href="index.php" ><h1>creador de marionetas</h1></a>
     </div>
     
 </div>
@@ -17,15 +18,28 @@
     <div class="contenido">
         
     </div>
-    
+  <div class="pie" >
+    <div class="contenidopie">
+       <a href="https://www.jccm.es/" target="_blank"
+          <img class="junta" rcs="imagenes/comunes/junta.jpg" 
+               alt="logo de la jccm" title="logo de jccm"/>
+          </a>
+       <a href="http://www.ajeclm.com/programa-tic/" target="_blank"
+          <img class="aje" rcs="imagenes/comunes/programatic.jpg" 
+               alt="programatic" title="programatic"/>
+          </a>
+        <a href="http://www.ajeclm.com/" target="_blank"
+          <img class="aje" rcs="imagenes/comunes/aje.jpg" 
+               alt="logo de aje" title="logo aje"/>
+          </a>
+        <a href="http://ec.europa.eu/esf/home.jsp?langId=es" target="_blank"
+            <img class="europeo" rcs="imagenes/comunes/europeo.jpg"
+               alt="logo del fondo social europeo" title="logo del fondo social europeo"/>
+          </a>
+    </div>
+  </div>  
 </div>
 
-<div class="pie" >
-    <div class="contenidopie">
-        
-    </div>
-    
-</div>
 </body>
 </html>
 
