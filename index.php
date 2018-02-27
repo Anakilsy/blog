29e58024a93067999d131e50c4305ab14d9d8de0
anakilsy <anita1997angel@gmail.com>
2018-02-27 13:16:23 +0100

    dia27

--- index.php
+++ index.php
@@ -16,24 +16,41 @@
 </div>
 <div class="fondo">
     <div class="contenido">
-        
+      <div class="entrada">
+        <div class="contenedorimagen">
+            <img class="imagen" src="imagenes/comunes/junta.jpg"   
+               alt="junta" title="junta"/>
+        </div>
+        <div class="informacion">
+           <p class="fecha">27 de Febrero de 2018</p>
+           <h2 class="titulo">ejenplo de titulo</h2>
+           <p class="texto"> Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat. Duis autem vel eum iriure dolor in hendrerit in vulputate velit esse molestie consequat, vel illum dolore eu feugiat nulla facilisis at vero eros et accumsan et iusto odio dignissim qui blandit praesent luptatum zzril delenit augue duis dolore te feugait nulla facilisi.
+
+Expetenda tincidunt in sed, ex partem placerat sea, porro commodo ex eam. His putant aeterno interesset at. Usu ea mundi tincidunt, omnium virtute aliquando ius ex. Ea aperiri sententiae duo. Usu nullam dolorum quaestio ei, sit vidit facilisis ea. Per ne impedit iracundia neglegentur. Consetetur neglegentur eum ut, vis animal legimus inimicus id.
+
+His audiam deserunt in, eum ubique voluptatibus te. In reque dicta usu. Ne rebum dissentiet eam, vim omnis deseruisse id. Ullum deleniti vituperata at quo, insolens complectitur te eos, ea pri dico munere propriae. Vel ferri facilis ut, qui paulo ridens praesent ad. Possim alterum qui cu. Accusamus consulatu ius te, cu decore soleat appareat usu.</p>
+           <form action="entrada.php" method="post" enctype="multinpart/form-data">
+            <input type="submit" class="boton leer" value="LEER MAS"/>
+           </form>
+        </div>
+      </div>  
     </div>
   <div class="pie" >
     <div class="contenidopie">
-       <a href="https://www.jccm.es/" target="_blank"
-          <img class="junta" rcs="imagenes/comunes/junta.jpg" 
+       <a href="https://www.jccm.es/" target="_blank">
+          <img class="junta" src="imagenes/comunes/junta.jpg"   
                alt="logo de la jccm" title="logo de jccm"/>
           </a>
-       <a href="http://www.ajeclm.com/programa-tic/" target="_blank"
-          <img class="aje" rcs="imagenes/comunes/programatic.jpg" 
+       <a href="http://www.ajeclm.com/programa-tic/" target="_blank">
+          <img class="programatic" src="imagenes/comunes/programatic.jpg" 
                alt="programatic" title="programatic"/>
           </a>
-        <a href="http://www.ajeclm.com/" target="_blank"
-          <img class="aje" rcs="imagenes/comunes/aje.jpg" 
+        <a href="http://www.ajeclm.com/" target="_blank">
+          <img class="aje" src="imagenes/comunes/aje.jpg" 
                alt="logo de aje" title="logo aje"/>
           </a>
-        <a href="http://ec.europa.eu/esf/home.jsp?langId=es" target="_blank"
-            <img class="europeo" rcs="imagenes/comunes/europeo.jpg"
+        <a href="http://ec.europa.eu/esf/home.jsp?langId=es" target="_blank">
+            <img class="europeo" src="imagenes/comunes/europeo.jpg"
                alt="logo del fondo social europeo" title="logo del fondo social europeo"/>
           </a>
     </div>
