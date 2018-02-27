284fa16b71663250b8961131ac4923c6a8bf3e67
anakilsy <anita1997angel@gmail.com>
2018-02-27 14:00:55 +0100

    dia27/14:00

---
+++ entrada.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+
+<html>
+<head>
+    <title>Blog</title>
+    <link href="estilos.css" rel="stylesheet" type="text/css"/>
+</head>
+
+<body>
+<div class="cabecera">
+    <div class="contenidocabecera">
+        <a href="http://www.ajeclm.com/" target="_blank" class="logo"></a>
+        <a href="index.php" ><h1>creador de marionetas</h1></a>
+    </div>
+    
+</div>
+<div class="fondoentrada">
+    <div class="contenidoentrada">
+       <a class="volver" href="index.php"><p>VOLVER</p></a>
+    </div>
+  <div class="pie" >
+    <div class="contenidopie">
+       <a href="https://www.jccm.es/" target="_blank">
+          <img class="junta" src="imagenes/comunes/junta.jpg"   
+               alt="logo de la jccm" title="logo de jccm"/>
+          </a>
+       <a href="http://www.ajeclm.com/programa-tic/" target="_blank">
+          <img class="programatic" src="imagenes/comunes/programatic.jpg" 
+               alt="programatic" title="programatic"/>
+          </a>
+        <a href="http://www.ajeclm.com/" target="_blank">
+          <img class="aje" src="imagenes/comunes/aje.jpg" 
+               alt="logo de aje" title="logo aje"/>
+          </a>
+        <a href="http://ec.europa.eu/esf/home.jsp?langId=es" target="_blank">
+            <img class="europeo" src="imagenes/comunes/europeo.jpg"
+               alt="logo del fondo social europeo" title="logo del fondo social europeo"/>
+          </a>
+    </div>
+  </div>  
+</div>
+
+</body>
+</html>
+
+
+<?php
+
+?>
