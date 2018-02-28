9396e60e015a7037433b3106b810617e59ae8481
anakilsy <anita1997angel@gmail.com>
2018-02-28 12:33:01 +0100

    dia 28

--- entrada.php
+++ entrada.php
@@ -17,6 +17,45 @@
 <div class="fondoentrada">
     <div class="contenidoentrada">
        <a class="volver" href="index.php"><p>VOLVER</p></a>
+       <div class="imagenentrada">
+            <img class="imagen" src="fotos/colauno.jpg"
+                     alt="junta" title="junta"/>
+        </div>
+        <p class="textoentrada">
+            <span class="fechaentrada">27 de Febrero de 2018</span>
+            <span class="huecoimagen"></span>Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+            Vestibulum sit amet interdum erat. Morbi in interdum sem. Vivamus condimentum pretium lobortis.
+            Praesent ac augue nunc. Morbi at nisl odio. Etiam luctus ipsum vel nisi posuere, vitae maximus
+            sem sagittis. Interdum et malesuada fames ac ante ipsum primis in faucibus. Nulla id posuere eros,
+            ut pulvinar odio. Aliquam vel neque id diam euismod volutpat id non lacus. In hac habitasse platea
+            dictumst.
+Praesent a arcu erat. Duis tincidunt tortor vel ultrices efficitur. Suspendisse tincidunt metus ut magna
+pharetra fermentum non nec enim. Etiam eu nulla venenatis, interdum risus eu, aliquet magna. Cras bibendum
+augue non ultricies porta. Donec non magna vel urna facilisis venenatis. Proin auctor rhoncus ex. Morbi
+placerat vestibulum rhoncus. Aenean porta quam dolor, in dapibus lacus bibendum in. Cras cursus erat at
+felis porttitor tincidunt. Vivamus odio orci, efficitur eget orci a, volutpat iaculis massa. Donec non
+ligula eu tortor elementum scelerisque non sit amet quam. Nunc vehicula a eros sit amet dapibus.
+
+Maecenas vel molestie sem, eget efficitur quam. Etiam pellentesque risus sit amet mauris sagittis pulvinar.
+Nulla congue tristique auctor. Sed vulputate ac sapien at rhoncus. Vivamus eu urna nunc. Ut tincidunt nunc at
+cursus ornare. Nam viverra ultrices est quis iaculis. Duis quis quam eget neque tincidunt ornare. Aliquam ac
+cursus felis. In augue orci, pharetra id finibus non, lacinia in massa. Cras fermentum arcu vel tincidunt
+dignissim. Fusce feugiat porta tortor a vehicula. Vestibulum ante ipsum primis in faucibus orci luctus et
+ultrices posuere cubilia Curae; Sed placerat justo ut nibh suscipit dignissim. Ut aliquam, elit sed luctus
+bibendum, sapien sapien accumsan nunc, ac ornare mi nisi eu ante.
+
+Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Praesent diam
+leo, sodales non ultrices eget, rutrum ac lacus. Mauris odio erat, fringilla sed neque vitae, lacinia pulvinar
+dui. Aliquam erat volutpat. Nunc nec tellus ipsum. Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+Nulla ac leo scelerisque, dapibus quam pulvinar, ultrices dolor. Pellentesque ultrices, lorem non tristique
+ornare, ipsum arcu porta mi, quis commodo velit nulla quis lectus. Duis nulla ante, congue eu luctus bibendum,
+gravida sit amet enim. Vivamus sed vestibulum arcu, posuere porta arcu.
+
+Nullam lobortis metus erat, et mollis augue euismod et. Sed gravida ligula lectus, sed tincidunt nisl fringilla
+in. In scelerisque, nibh vel pellentesque convallis, lorem mi facilisis elit, vel placerat nulla magna eu neque.
+Cras lacinia sit amet diam nec condimentum. Morbi eu arcu vel mauris pretium vulputate a imperdiet ante. Sed et
+felis et diam molestie condimentum. Duis sit amet quam ipsum. Nullam fermentum vehicula rhoncus.
+</p>
     </div>
   <div class="pie" >
     <div class="contenidopie">
